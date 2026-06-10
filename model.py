16e21fa8afd2270acf5c1211f4cb3f9da1f6bac2
BoarArtem <workboar@gmail.com>
2026-06-10 12:41:02 +0300

    fixing

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 def train_model():
     model = YOLO("yolo26n.pt")
 
-    model.train(data="data/data.yaml", epochs=20)
+    model.train(data="data/data.yaml", epochs=100)
 
 train_model()
 
