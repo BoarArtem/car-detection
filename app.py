b29e34d38fd2ef9cd32cc4c87b4d895c480b5128
BoarArtem <workboar@gmail.com>
2026-06-13 21:59:01 +0300

    Translate UI text from Russian to English

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ if "running" not in st.session_state:
 
 st.title("Realtime car detection")
 
-yt_url = st.text_input("Введите ссылку на трансляцию в ютубе")
+yt_url = st.text_input("Enter the link to the YouTube broadcast")
 
 confidence = st.slider(
-    "Уверенеость модели",
+    "Model confidence",
     0.1,
     1.0,
     0.4,
@@ -19,20 +19,20 @@ confidence = st.slider(
 
 col1, col2 = st.columns(2)
 
-with col1:
-    if st.button("Запустить"):
+with col1:      
+    if st.button("Start"):
         st.session_state.running = True
 with col2:
-    if st.button("Остановить"):
+    if st.button("Stop"):
         st.session_state.running = False
 
 
 frame_placeholder = st.empty()
 
 if st.session_state.running:
-    st.write("Получение потока...")
+    st.write("Receiving stream...")
     stream_url = get_stream_url(yt_url)
-    st.success(f"Поток подключен: {stream_url}")
+    st.success(f"Stream connected: {stream_url}")
 
     cap = cv2.VideoCapture(stream_url)
 
@@ -46,6 +46,11 @@ if st.session_state.running:
             annotated_frame,
             cv2.COLOR_BGR2RGB
         )
+
+        cars_number = len(result[0].boxes)
+
+        cv2.putText(annotated_frame, f"Cars: {cars_number}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
         frame_placeholder.image(
             annotated_frame,
             channels="RGB",
